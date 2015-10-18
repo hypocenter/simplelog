1fc8f8b8d8242278191c729a26537207fc6b02a0
hypo <hypomail@gmail.com>
2015-10-18 21:24:31 +0800

    加入一些测试代码

--- simplelog_test.go
+++ simplelog_test.go
@@ -1,1 +1,44 @@
-package test
+package simplelog
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestDefaultStdout(t *testing.T) {
+	Trace("Some text printed by Trace()")
+	Debug("Some text printed by Debug()")
+	Info("Some text printed by Info()")
+	Warning("Some text printed by Warning()")
+	Error("Some text printed by Error()")
+	Flush()
+}
+
+func TestDefaultReturnValue(t *testing.T) {
+	err := Warning("Some text printed by Warning()")
+	if err == nil || err.Error() != "[WARNING] Some text printed by Warning()" {
+		t.Error("Warning() return wrong value.")
+	}
+
+	err = Error("Some text printed by Error()")
+	if err == nil || err.Error() != "[ERROR] Some text printed by Error()" {
+		t.Error("Error() return wrong value.")
+	}
+	Flush()
+}
+
+func TestCustomLog(t *testing.T) {
+	buf := []byte{}
+	writer := bytes.NewBuffer(buf)
+	logger := New()
+	logger.AddWriter(writer, L_DEBUG, W_BOTH)
+
+}
+
+func TestCustomLogWay(t *testing.T) {
+
+}
+
+func TestCustomLogPrefix(t *testing.T) {
+
+}
